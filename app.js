c264084b2587832c517d98fdfa6d8c596af26531
Mark Anderson <markcanderson@student.midlandstech.edu>
2022-04-28 16:24:16 -0400

    added upper case letter to end of password

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 // App that creates a random password for a given size
+const { groupEnd } = require("console");
 const { rand } = require("elliptic");
 const prompt = require("prompt-sync")();
 
-// loops through list and randomly selects item
 const passwordSize = prompt("Password size: ");
+
+// loops through list and randomly selects item
+
 function generator(passwordSize) {
   const char = [
     "a",
@@ -46,8 +49,9 @@ function generator(passwordSize) {
     "!",
   ];
 
-  let twoUpper = "";
-  let twoUpper2 = "";
+  // Infinite loop that runs a random generator for a password
+  // if the password includes "@" and "!" then the while loop
+  // breaks and returns the result
 
   while (true) {
     let result = "";
@@ -60,6 +64,12 @@ function generator(passwordSize) {
     }
   }
 }
+
+// This function is run when the "Get Password" button is
+// clicked and runs a verification to check is size is a
+// number and if it is greater than 4 and less that 21 if it
+// is the size is accepted and youbreak out of the loop after
+// printing out on the webpage the password
 
 function getPassword() {
   let size = document.getElementById("size").value;
@@ -78,13 +88,20 @@ function getPassword() {
       break;
     } else {
       let result = generator(size);
+      // New Lines
+      let y = myUpper(result);
+      let newPass = rebuildPassword(result, y);
+      // New Lines
       let password = document.querySelector("p");
       password.style = "color:rgb(38, 39, 72); font-size:1.25em";
-      password.innerHTML = result;
+      // was result
+      password.innerHTML = newPass;
       break;
     }
   }
 }
+
+// clears the password off the webpage
 
 function reset() {
   let clear = document.querySelector("p");
@@ -92,6 +109,25 @@ function reset() {
   blank();
 }
 
+// clears the number in the input box to a blank box
+
 function blank() {
   document.getElementById("size").value = "";
+}
+
+// function returns a single uppercase letter from the password
+function myUpper(num) {
+  for (let i = 0; i < num.length; i++) {
+    if (num[i] != "@" && num[i] != "!" && num[i] != Number(num[i])) {
+      let myUpper = num[i].toUpperCase();
+      return myUpper;
+    }
+  }
+}
+
+// function return the password rebuilt with an uppercase letter
+function rebuildPassword(result, y) {
+  let z = result.slice(1);
+  let newPass = z + y;
+  return newPass;
 }
